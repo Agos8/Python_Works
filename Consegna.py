15ab51cdbc9fc08efd68ddc410501c0dfbc83e84
Agos8 <108658816+Agos8@users.noreply.github.com>
2022-07-04 12:52:15 +0200

    Add files via upload

---
+++ Consegna.py
@@ -0,0 +1,13 @@
+a = input("Inserisci il dato")
+if(a != False):
+   print(a)
+
+Lista = (1, 2, 3)
+if(Lista != False):
+       print(Lista)
+
+Nome = input("Come ti chiami?")
+Citta = input("Da dove vieni?")
+if(Nome != "Agostino" and Citta != "Milano"):
+    Lista = (Nome, Citta)
+    print(Lista)
